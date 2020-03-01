d6d5c976e5d2105f8a868e115accd84bbd28971c
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-01 23:47:00 +0000

    Add test for airport land method

--- src/airport.py
+++ src/airport.py
@@ -2,5 +2,9 @@ class Airport:
     def __init__(self):
         self.planes = []
 
-    def get_planes():
+    def get_planes(self):
         return self.planes
+        
+    def land(self, plane):
+        self.planes.append(plane)
+        return plane
--- tests/test_airport.py
+++ tests/test_airport.py
@@ -1,6 +1,18 @@
+import pytest
+from unittest.mock import MagicMock
 from src.airport import Airport
+from src.plane import Plane
+
+@pytest.fixture
+def mock_plane():
+    return MagicMock(spec=Plane)
 
 def test_no_planes_by_default():
-  airport = Airport()
-  assert airport.planes == []
-  
+    airport = Airport()
+    
+    assert airport.get_planes() == []
+
+def test_land_plane(mock_plane):
+    airport = Airport()
+    
+    assert airport.land(mock_plane) == mock_plane
